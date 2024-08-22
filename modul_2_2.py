6e2bda31730d5657232556286c3349a4e24131c6
algulyaev61 <alex1961gulyaev@gmail.com>
2024-08-22 10:49:28 +0300

    Add files via upload

---
+++ modul_2_2.py
@@ -0,0 +1,9 @@
+first = int(input('Введите 1 число: '))
+second = int(input('Введите 2 число: '))
+third = int(input('Введите 3 число: '))
+if first==second==third:
+    print(3)
+elif first!=second!=third:
+    print(0)
+else:
+    print(2)
