b88efa984181d5f5f079cecddd59c3117b030f61
algulyaev61 <alex1961gulyaev@gmail.com>
2024-08-22 13:46:00 +0300

    Update modul_2_2.py

--- modul_2_2.py
+++ modul_2_2.py
@@ -3,7 +3,7 @@ second = int(input('Введите 2 число: '))
 third = int(input('Введите 3 число: '))
 if first==second==third:
     print(3)
-elif first!=second!=third:
-    print(0)
-else:
+elif first!=second!=third or first != second == third or first == third != second:
     print(2)
+else:
+    print(0)
